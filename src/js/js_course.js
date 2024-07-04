756a817033666f06f571384746e2cfa827928f6f
Volodymyr <vhubash@gmail.com>
2024-07-04 13:56:20 +0300

    upd

--- src/js/js_course.js
+++ src/js/js_course.js
@@ -47,3 +47,28 @@ if (lessonBtn) {
 
 
 
+async function fetchEvents(name) {
+  try {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyDdXCV34b6Y1fWpi7H-TKahOOe2olSkE6emOr8aLXZ6XpVGHMrajAvA08bAUbd4tE-EA/exec');
+  const events = await response.json();
+  const eventsList = document.getElementById('events');
+  // Знайти найближчу подію з назвою "Фронтенд - Саша"
+  const targetEvent = events
+    .filter(event => event.title === name)
+    .sort((a, b) => new Date(a.startTime) - new Date(b.startTime))[0];
+
+  if (targetEvent) {
+    const eventDate = new Date(targetEvent.startTime);
+    const formattedDate = eventDate.toLocaleDateString('uk-UA', { day: 'numeric', month: 'long' });
+    const formattedTime = eventDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+
+    // Оновити HTML-елемент
+    document.getElementById('eventDate').innerHTML += formattedDate;
+    document.getElementById('eventTime').innerHTML += formattedTime;
+  } else {
+    console.log('Подія', name,' не знайдена');
+  }
+} catch (error) {
+  console.error('Error fetching events:', error);
+}}
