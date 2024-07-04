da019cce7920e7e8db174fcb2c71746bcfc5e5a2
Volodymyr <vhubash@gmail.com>
2024-07-04 06:08:29 +0300

    add lesson 1-2 frontend

--- src/js/js_course.js
+++ src/js/js_course.js
@@ -10,33 +10,40 @@ function slowScroll(id) {
 
 let phoneBtn = document.getElementById("phonebtn");
 let lessonBtn = document.getElementById("lessonBtn");
+if (phoneBtn) {
+  phoneBtn.addEventListener("mouseenter", function (event) {
+    // highlight the mouseenter target
+    event.target.classList.add("heartBeat");
+    event.target.style.color = '#ffbf00';
+  }, false);
+  phoneBtn.addEventListener("mouseleave", function (event) {
+    // highlight the mouseenter target
+    event.target.classList.remove("heartBeat");
+    event.target.style.color = '#f2f7f7';
+  }, false);
+  
+}
 
-phoneBtn.addEventListener("mouseenter", function (event) {
-  // highlight the mouseenter target
-  event.target.classList.add("heartBeat");
-  event.target.style.color = '#ffbf00';
-}, false);
-phoneBtn.addEventListener("mouseleave", function (event) {
-  // highlight the mouseenter target
-  event.target.classList.remove("heartBeat");
-  event.target.style.color = '#f2f7f7';
-}, false);
+if (lessonBtn) {
+  lessonBtn.addEventListener("mouseenter", function (event) {
+    // highlight the mouseenter target
+    event.target.style.transform = 'scale(1.1)';
+  }, false);
+  lessonBtn.addEventListener("mouseleave", function (event) {
+    // highlight the mouseenter target
+    event.target.style.transform = 'scale(1.0)';
+  }, false);
+  
+}
+// if (arrowTopBtn){
+//   arrowTopBtn.onclick = function() {
+//     window.scrollTo(pageXOffset, 0);
+//   };
+ 
+//   window.addEventListener('scroll', function() {
+//     arrowTopBtn.hidden = (pageYOffset < document.documentElement.clientHeight);
+//   });
+// }
 
-lessonBtn.addEventListener("mouseenter", function (event) {
-  // highlight the mouseenter target
-  event.target.style.transform = 'scale(1.1)';
-}, false);
-lessonBtn.addEventListener("mouseleave", function (event) {
-  // highlight the mouseenter target
-  event.target.style.transform = 'scale(1.0)';
-}, false);
-
-arrowTopBtn.onclick = function() {
-   window.scrollTo(pageXOffset, 0);
- };
-
- window.addEventListener('scroll', function() {
-   arrowTopBtn.hidden = (pageYOffset < document.documentElement.clientHeight);
- });
 
 
